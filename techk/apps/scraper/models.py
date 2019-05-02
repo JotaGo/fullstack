7424929752b4dd56339310c79617027d6681803b
Juan Marquez <jemarqueza1993@hotmail.com>
2019-05-02 14:52:35 -0400

    Diseño opcional de base de datos

--- techk/apps/scraper/models.py
+++ techk/apps/scraper/models.py
@@ -4,3 +4,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Libro(models.Model):
+    category = models.CharField(max_length=20)
+    title = models.CharField(max_length= 100)
+    thumbnail = models.URLField(max_length=200)
+    price = models.CharField(max_length=8)
+    stock = models.CharField(max_length=4)
+    product_description = models.TextField()
+    upc = models.CharField(max_length=20)
--- techk/config/urls.py
+++ techk/config/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from apps.base.views import index
+from apps.scraper.views import scraper
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'scraper/', scraper),
     url(r'', index),
     url(r'^api-auth/', include('rest_framework.urls'))
 ]
